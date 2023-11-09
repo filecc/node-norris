1473ff610f5ac912875b34509dd3c2cfd9db7a90
filecc <fil.gia@live.it>
2023-11-09 14:28:37 +0100

    added env file with PORT

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const http = require('http')
-const fs = require('fs')
-const path = require("path");
+require('dotenv').config()
 const { addJokeToFileIfNotExists } = require('./utils/functions')
-const url = "https://api.chucknorris.io/jokes/random"
-
 
 
 const server = http.createServer((req, res) => {
+    const url = "https://api.chucknorris.io/jokes/random"
     // remove favicon request
     if(req.url === '/favicon.ico') {
         res.writeHead(200, { "Content-Type": "image/x-icon" });
@@ -25,11 +23,11 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         res.write(
           `<main style="padding: 2rem;">
-          <div style="text-align: center">
-                <img style="max-width: 300px" src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" />
-          </div>
-          <h1>Your Daily Chuck Norris Phrase:</h1>
-          <h2 style="font-weight: light">${data.value}</h2>
+            <div style="text-align: center">
+                 <img style="max-width: 300px" src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" />
+            </div>
+            <h1>Your Daily Chuck Norris Phrase:</h1>
+            <h2 style="font-weight: light">${data.value}</h2>
           </main>`
         );
         res.end();
@@ -38,6 +36,6 @@ const server = http.createServer((req, res) => {
 
 
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000')
+server.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`)
 })
